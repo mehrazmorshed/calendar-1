6afc86d5883ac1de3ebba8de146eab6e2ba9e7d3
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-01-10 14:56:35 +0600

    last day of previous month

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ const months = ["January", "February", "March", "April", "May", "June",
 	"July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
-	let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // getting last date of month
+	let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+	lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+	lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
 	let liTag = "";
+
+	for (let i = firstDayofMonth; i > 0; i--) {
+		liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+
+	}
 
 	for (let i = 1; i <= lastDateofMonth; i++) {
 		liTag += `<li>${i}</li>`;
